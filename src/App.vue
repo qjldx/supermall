a10e9532abc4a955aa24b79447ee6460f695dd4a
qjldx <qjldx@126.com>
2020-11-06 00:37:34 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,20 @@
 <template>
   <div id="app">
-    <tab-bar>
-    <tab-bar-item path="/home" activeColor="red">
-      <img slot="item-icon" src="./assets/img/tabbar/home.png">
-      <img slot="item-icon-active" src="./assets/img/tabbar/home_active.png">
-      <div slot="item-text">首页</div>
-    </tab-bar-item>
-    <tab-bar-item path="/category" activeColor="#16f6f6">
-      <img slot="item-icon" src="./assets/img/tabbar/category.png">
-      <img slot="item-icon-active" src="./assets/img/tabbar/category_active.png">
-      <div slot="item-text">分类</div>
-    </tab-bar-item>
-    <tab-bar-item path="/cart" activeColor="green">
-      <img slot="item-icon" src="./assets/img/tabbar/mall.png">
-      <img slot="item-icon-active" src="./assets/img/tabbar/mall_active.png">
-      <div slot="item-text">购物车</div>
-
-    </tab-bar-item>
-    <tab-bar-item path="/profile" activeColor="cyan">
-      <img slot="item-icon" src="./assets/img/tabbar/mine.png">
-      <img slot="item-icon-active" src="./assets/img/tabbar/mine_active.png">
-      <div slot="item-text">我的</div>
-    </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import TabBar from "./components/tabbar/TabBar";
-  import TabBarItem from "./components/tabbar/TabBarItem";
+  import MainTabBar from "./components/content/MainTabBar";
   export default {
-    components: {TabBarItem, TabBar}
+    components: {
+      MainTabBar
+    }
   }
 </script>
 <style scoped>
-
+/*导入base.css文件*/
+  @import "assets/css/base.css";
 
 </style>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <!--这个地方去掉了div这个标签了 加上不知道可不可以-->
+      <tab-bar>
+        <tab-bar-item path="/home" activeColor="red">
+          <img slot="item-icon" src="~assets/img/tabbar/home.png">
+          <!--在dom文件中加上别名是要采用~符号-->
+         <!-- <img slot="item-icon" src="~assets/img/tabbar/home.svg">-->
+
+          <img slot="item-icon-active" src="~assets/img/tabbar/home_active.png">
+          <div slot="item-text">首页</div>
+        </tab-bar-item>
+        <tab-bar-item path="/category" activeColor="#16f6f6">
+          <img slot="item-icon" src="~assets/img/tabbar/category.png">
+          <img slot="item-icon-active" src="~assets/img/tabbar/category_active.png">
+          <div slot="item-text">分类</div>
+        </tab-bar-item>
+        <tab-bar-item path="/cart" activeColor="green">
+          <img slot="item-icon" src="~assets/img/tabbar/mall.png">
+          <img slot="item-icon-active" src="~assets/img/tabbar/mall_active.png">
+          <div slot="item-text">购物车</div>
+
+        </tab-bar-item>
+        <tab-bar-item path="/profile" activeColor="cyan">
+          <img slot="item-icon" src="~assets/img/tabbar/mine.png">
+          <img slot="item-icon-active" src="~/assets/img/tabbar/mine_active.png">
+          <div slot="item-text">我的</div>
+        </tab-bar-item>
+      </tab-bar>
+
+</template>
+
+<script>
+  import TabBarItem from "../tabbar/TabBarItem";
+  import TabBar from "../tabbar/TabBar";
+    export default {
+        name: "MainTabBar",
+      components:{
+          TabBar,TabBarItem
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
